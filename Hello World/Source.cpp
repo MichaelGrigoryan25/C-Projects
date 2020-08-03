70debed22f45674c547e73919015014b75cfbf1f
Michael <michael.grigoryan25@gmail.com>
2020-08-03 10:58:08 +0400

    Added data types

--- Hello World/Source.cpp
+++ Hello World/Source.cpp
@@ -1,17 +1,42 @@
-// My First C++ Program
-
+//
 #include <iostream>
+
+//
 using namespace std;
 
+// This block will get executed first. Then eveything in it.
 int main() {
 
+     ///////////////////////////////////////
+    //          Using Variables          //
+   ///////////////////////////////////////
     string MyName;
     int MyAge;
 
+    // Define Variable Values
     MyName = "Mike";
     MyAge = 15;
 
+    // Print the line
     cout << "Hello my name is " << MyName << ". I'm " << MyAge << " years old." << endl;
+   
+    //----------------------------------------------------------------------------------
+
+    ///////////////////////////////////////
+   //            Data Types             //
+  ///////////////////////////////////////
+
+    char grade = 'A'; // Character
+    string phrase = "Michael Grigoryan"; // String
+    int age = 50; // Integer Number
+    float g = 9.8; // Decimal(Floating) Number
+    double gpa = 4.5; // Very Specific Number 
+    bool  isMale = true; // Boolean(true/false)
+
+    cout << NULL << endl;
+
+
+
 
     return 0;
 }
