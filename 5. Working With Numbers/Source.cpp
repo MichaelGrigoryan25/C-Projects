431ec00cfcae7ee06a652787d28ba96868d25e8f
Michael <michael.grigoryan25@gmail.com>
2020-08-03 19:27:23 +0400

    Fixed a solution bug

--- 5. Working With Numbers/Source.cpp
+++ 5. Working With Numbers/Source.cpp
@@ -7,19 +7,19 @@ int main()
 	///////////////////////////////////////
    //     Working With Numbers          //
   ///////////////////////////////////////
-	
-	cout << 40; // Will print 40
-	cout << -40; // Will print -40
-	cout << 1 + 2; // 3
-	cout << 2 - 1; // 1
-	cout << 2 * 1; // 2
-	cout << 2 / 1; // 2
-	cout << 10 % 3; // 1 = 10/3 = 3+1/3
-	cout << (1 + 2); *10 // 30
 
-	int IncrementedNumber = 10;
-	IncrementedNumber++;
-	cout << IncrementedNumber;
+	cout << 40 << endl; // Will print 40
+	cout << -40 << endl; // Will print -40
+	cout << 1 + 2 << endl; // 3
+	cout << 2 - 1 << endl; // 1
+	cout << 2 * 1 << endl; // 2
+	cout << 2 / 1 << endl; // 2
+	cout << 10 % 3 << endl; // 1 = 10/3 = 3+1/3
+	cout << (1 + 2) * 10 << endl; // 30
+
+	int wnum = 10;
+	wnum++;
+	cout << wnum << endl;
 
 	return 0;
 }
