81f61034db8f99c4f2bacfca8c4b29541e1bb6da
Michael <michael.grigoryan25@gmail.com>
2020-08-04 01:29:32 +0400

    Fix some files

---
+++ 8. Build a Calculator/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+
+	cout << "Enter the first number: " << endl;
+	cin >> num1;
+
+	cout << "Enter the Second number: " << endl;
+	cin >> num2;
+
+	cout << num1 + num2 << endl;
+
+	return 0;
+}
