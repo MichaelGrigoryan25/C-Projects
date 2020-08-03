8e7e6d2dda8fb8e2b56159076de95674b7ac065f
Michael <michael.grigoryan25@gmail.com>
2020-08-04 00:45:23 +0400

    Added a new file

--- 6. Using cmath Module/Source.cpp
+++ 6. Using cmath Module/Source.cpp
@@ -4,12 +4,13 @@
 using namespace std;
 int main()
 {
-	cout << sqrt(36);   //
-	cout << pow(3, 2);  // 
-	cout << round(4.6); // Round a number (Follows normal rounding rules) -> Will round 4.6 -> 5.0
-	cout << floor(4.3);	// Will round the number down
-	cout << ceil(4.35); // Will round the number up
-	cout << fmax(3, 10);// Will return the bigger number. In this case --> 10
+	cout << sqrt(36);		// Square root
+	cout << pow(3, 2);		//  3 ^ 2
+	cout << round(4.6);		// Round a number (Follows normal rounding rules) -> Will round 4.6 -> 5.0
+	cout << floor(4.3);		// Will round the number down
+	cout << ceil(4.35);		// Will round the number up
+	cout << fmax(3, 10);	// Will return the bigger number. In this case --> 10
+	cout << fmin(3, 10);	// Will return the smallest number
 
 	return 0;
 }
---
+++ 7. Getting User Input/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int age;
+
+	cout << "Enter your age: ";					 // Returning the message
+
+	cin >> age;									 // Asking the user for his age
+
+	cout << "You are " << age << " years old";   // Returning the sentence
+
+	return 0;
+}
