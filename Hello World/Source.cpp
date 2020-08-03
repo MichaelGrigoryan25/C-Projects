c7aee0725fa28d002d52d5934ee69350d6b9f184
Michael <michael.grigoryan25@gmail.com>
2020-08-03 10:45:02 +0400

    Initial Commit

---
+++ Hello World/Source.cpp
@@ -0,0 +1,17 @@
+// My First C++ Program
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string MyName;
+    int MyAge;
+
+    MyName = "Mike";
+    MyAge = 15;
+
+    cout << "Hello my name is " << MyName << ". I'm " << MyAge << " years old." << endl;
+
+    return 0;
+}
