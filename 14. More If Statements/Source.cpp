99e974bc84e61bec44a8bc2a19b2aed1dd4872a5
Michael <michael.grigoryan25@gmail.com>
2020-08-04 17:03:17 +0400

    Added new section

---
+++ 14. More If Statements/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Created a getMax Function
+
+int getMax(int num1, int num2, int num3)
+{
+	int result;
+
+	if (num1 >= num2 && num1 >= num3)
+	{
+		result = num1;
+	}
+	else if (num2 >= num1 && num2 >= num3)
+	{
+		result = num2;
+	}
+	else
+	{
+		result = num3;
+	}
+
+	return result;
+}
+
+int main() 
+{
+	cout << getMax(10, 5, 1); // Will print 10. (10 > 5 > 1)
+
+	return 0;
+}
---
+++ 15. Building a Better Calculator/Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+	char op;
+	
+	cout << "Enter the first number: ";
+	cin >> num1;
+
+	cout << "Enter the operation: ";
+	cin >> op;
+
+	cout << "Enter the second number: ";
+	cin >> num2;
+
+	if (op == '+')
+	{
+		cout << num1 + num2;
+	}
+	else if (op == '-')
+	{
+		cout << num1 - num2;
+	}
+	else if (op == '/')
+	{
+		cout << num1 / num2;
+	}
+	else if (op == '*')
+	{
+		cout << num1 * num2;
+	}
+	else 
+	{
+		cout << "Invalid Operator";
+	}
+
+	return 0;
+}
