df03bc0c7a7dc7dc40b4a339f622b64d0ff01f50
Michael <michael.grigoryan25@gmail.com>
2020-08-05 18:56:04 +0400

    Added more code

---
+++ 19. For Loops/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int nums[] = { 1,2,5,7,3 };
+
+	for (int i = 0; i < 5; i++) // Will loop 5 times, i = 0 -> i < 5
+	{
+		cout << nums[i] << endl;
+		//1
+		//2
+		//5
+		//7
+		//3
+	}
+
+	return 0;
+}
